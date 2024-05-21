a77c1fc6dfde4499ac1c7be2a13d8c809bead41d
krupa-sri <krupa.sri1@outlook.com>
2024-05-21 17:13:28 +0530

    completed todolist app

--- todo-list/my-react-app/src/ToDoList.jsx
+++ todo-list/my-react-app/src/ToDoList.jsx
@@ -1,59 +1,80 @@
 import React, { useState } from "react";
 
 function ToDoList() {
-  const [tasks, setTasks] = useState(["Brush your teeth","Take shower","Eat breakfast"]);
+  const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState("");
 
   function handleInputChange(event) {
-
     setNewTask(event.target.value);
   }
 
-  function addTask() {}
+  function addTask() {
+    if (newTask.trim() !== "") {
+      setTasks((t) => [...t, newTask]);
+      setNewTask("");
+    }
+  }
 
-  function deleteTask(index) {}
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
 
-  function moveTaskUp(index) {}
+  function moveTaskUp(index) {
+    if (index > 0) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index - 1]] = [
+        updatedTasks[index - 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
 
-  function moveTaskDown(indeex) {}
+  function moveTaskDown(index) {
+    if (index <tasks.length-1) {
+      const updatedTasks = [...tasks];
+      [updatedTasks[index], updatedTasks[index + 1]] = [
+        updatedTasks[index + 1],
+        updatedTasks[index],
+      ];
+      setTasks(updatedTasks);
+    }
+  }
 
   return (
     <div className="to-do-list">
       <h1>To-Do-List</h1>
 
       <div>
-        <input 
-        type="text" 
-        placeholder="Enter a task...." 
-        value={newTask}
-        onChange={handleInputChange} />
-        <button 
-        className="add-button"
-        onClick={addTask}>
-            Add
+        <input
+          type="text"
+          placeholder="Enter a task...."
+          value={newTask}
+          onChange={handleInputChange}
+        />
+        <button className="add-button" onClick={addTask}>
+          Add
         </button>
       </div>
 
       <ol>
-        {tasks.map((task,index)=> 
-        <li key={index}>
+        {tasks.map((task, index) => (
+          <li key={index}>
             <span className="text">{task}</span>
-            <button className="delete-button"
-            onClick={ () => deleteTask(index)}>
-                Delete
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              Delete
             </button>
 
-            <button className="move-button"
-            onClick={ () => moveTaskUp(index)}>
-                up
+            <button className="move-button" onClick={() => moveTaskUp(index)}>
+              up
             </button>
 
-            <button className="move-button"
-            onClick={ () => moveTaskDown(index)}>
-                down
+            <button className="move-button" onClick={() => moveTaskDown(index)}>
+              down
             </button>
-        </li>
-        )}
+          </li>
+        ))}
       </ol>
     </div>
   );

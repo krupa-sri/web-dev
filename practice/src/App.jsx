04f0194d517c03b628a9390adf161e8ad9f3c7f3
krupa-sri <krupa.sri1@outlook.com>
2024-06-12 15:58:09 +0530

    learning use effect

---
+++ practice/src/App.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [time, setTime] = useState(new Date().toString());
+  const [message, setMessage] = useState("functional components");
+
+  useEffect(() => {
+    console.log("componented monted or updated");
+    const interval = setInterval(showDate, 1000);
+    return() =>{
+      console.log("cleanup of interval");
+      clearInterval();
+    }
+  },[time]);
+  const showDate = () => {
+    setTime(new Date().toString());
+  };
+
+  return (
+    <div>
+      <div>{time}</div>
+      <button onClick={showDate}>Show Date </button>
+      <div>{message}</div>
+      <button onClick={() => setMessage("changed functional components")}>
+        Change Button{" "}
+      </button>
+    </div>
+  );
+};
+
+export default App;
